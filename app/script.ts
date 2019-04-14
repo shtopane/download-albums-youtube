75ada7c3f7bee28126369913342d3bd9bc58337a
kkirilova <vechesumstudent@gmail.com>
2019-04-14 11:15:35 +0300

    formatted all functions; all have args types and return types

--- app/script.ts
+++ app/script.ts
@@ -1,138 +1,145 @@
-    const convertBtn = document.querySelector('.convert-button');
-    const inputEl: HTMLInputElement = document.querySelector('.URL-input');
-    
-    const textareaEl: HTMLTextAreaElement = document.querySelector('#description');
-    const loader = document.getElementById('loader');
-    const container = document.getElementById('container');
-    const playlistEl = document.getElementById('playlist');
+import { Playlist } from "./models/playlist";
+import { TracklistInfo } from "./models/tracklist-info";
 
-    convertBtn.addEventListener('click', function () {
-        let start = Date.now();
-        const url = inputEl.value;
-        const body = JSON.stringify({ playlist: textareaEl.value, url: url });
-       
-        toggleLoader(true);
+const serverUrl = 'http://localhost:4000';
 
-        fetch('http://localhost:4000/songs', {
-            method: 'POST',
-            body: body,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(json => {
-            toggleLoader(false);
-            console.log(`done in - ${(Date.now() - start) / 1000}s`);
+const convertBtn: HTMLButtonElement = document.querySelector('.convert-button');
+const inputEl: HTMLInputElement = document.querySelector('.URL-input');
+const textareaEl: HTMLTextAreaElement = document.querySelector('#description');
+const loader = document.getElementById('loader');
+const container = document.getElementById('container');
+const playlistEl = document.getElementById('playlist');
 
-            fetch('http://localhost:4000/playlist').then(res => res.json()).then(albumInfo => {
-                console.log('playlist from server');
-                generatePlaylist(albumInfo.playlist, albumInfo.albumName);
-                addDownloadLinks(albumInfo.albumName);
-            });
-        });
-    });
-
-    function toggleLoader(startLoader){
-        if(startLoader){
-            loader.classList.remove('hidden');
-            container.classList.add('blur');
-        }else{
-            loader.classList.add('hidden');
-            container.classList.remove('blur');
-        }
-    }
-
-   function addDownloadLinks(albumName){
-        const downloadBtnCollection = document.querySelectorAll('.download-btn');
-        for(let downloadBtn of downloadBtnCollection) {
-            downloadBtn.addEventListener('click', function(event){
-                const target: HTMLButtonElement = <HTMLButtonElement> event.target;
-                window.location.href = `http://localhost:4000/download?albumName=${albumName}&songName=${target.id}`;
-            });
-        }
-   }
-
-    const playlist = {
-        "playlist": [
-          {
+/** TEST DATA */
+const playlist: TracklistInfo = {
+    "playlist": [
+        {
             "songBegin": "00:00",
             "songName": "Muddy waters",
             "tumbnail": "https://i.ytimg.com/vi/hsUimLUG7U4/default.jpg"
-          },
-          {
+        },
+        {
             "songBegin": "03:45",
             "songName": "No witness",
             "tumbnail": "https://i.ytimg.com/vi/hsUimLUG7U4/default.jpg"
-          },
-          {
+        },
+        {
             "songBegin": "07:08",
             "songName": "Lost on you",
             "tumbnail": "https://i.ytimg.com/vi/hsUimLUG7U4/default.jpg"
-          },
-          {
+        },
+        {
             "songBegin": "11:30",
             "songName": "Up against me",
             "tumbnail": "https://i.ytimg.com/vi/hsUimLUG7U4/default.jpg"
-          },
-          {
+        },
+        {
             "songBegin": "14:27",
             "songName": "Tightrope",
             "tumbnail": "https://i.ytimg.com/vi/hsUimLUG7U4/default.jpg"
-          },
-          {
+        },
+        {
             "songBegin": "17:55",
             "songName": "Other people",
             "tumbnail": "https://i.ytimg.com/vi/hsUimLUG7U4/default.jpg"
-          },
-          {
+        },
+        {
             "songBegin": "21:51",
             "songName": "Into the wild",
             "tumbnail": "https://i.ytimg.com/vi/hsUimLUG7U4/default.jpg"
-          },
-          {
+        },
+        {
             "songBegin": "25:26",
             "songName": "Strange",
             "tumbnail": "https://i.ytimg.com/vi/hsUimLUG7U4/default.jpg"
-          },
-          {
+        },
+        {
             "songBegin": "29:12",
             "songName": "Death Valley",
             "tumbnail": "https://i.ytimg.com/vi/hsUimLUG7U4/default.jpg"
-          },
-          {
+        },
+        {
             "songBegin": "31:55",
             "songName": "You want it all",
             "tumbnail": "https://i.ytimg.com/vi/hsUimLUG7U4/default.jpg"
-          }
-        ],
-        "albumName": "LP - Lost On You (Album NoStop Mix)"
-      }
+        }
+    ],
+    "albumName": "LP - Lost On You (Album NoStop Mix)"
+};
 
-      function generatePlaylist(playlistArr, albumName){
-          const tracklistEl = document.querySelector('.tracklist');
-          let albumTitle = <Element> playlistEl.querySelector('h2').cloneNode();
-          albumTitle.textContent = albumName;
-          albumTitle.classList.remove('hidden');
-          let tracklistCopy;
-          let currentTrack;
-          let downloadBtn;
-          let tumbnailEl;
-          let songEl;
+convertBtn.addEventListener('click', () => {
+    let start = Date.now();
+    const url = inputEl.value;
+    const body = JSON.stringify({ playlist: textareaEl.value, url: url });
 
-          for(let i = 0; i < playlistArr.length; i++){
-              currentTrack = playlistArr[i];
-              tracklistCopy = tracklistEl.cloneNode(true);
-              tracklistCopy.classList.remove('hidden');
-              tumbnailEl = tracklistCopy.querySelector('img');
-              songEl = tracklistCopy.querySelector('.song-info');
-              downloadBtn = tracklistCopy.querySelector('.download-btn');
-              downloadBtn.id = currentTrack.songName;
-              songEl.textContent = `${currentTrack.songName} - ${currentTrack.songBegin}`;
-              tumbnailEl.src = currentTrack.tumbnail;
-              
-              console.log(tracklistCopy);
-              playlistEl.classList.remove('hidden');
-              playlistEl.appendChild(tracklistCopy);
-          }
-      }
+    toggleLoader(true);
 
-    // generatePlaylist(playlist.playlist, playlist.albumName);
+    fetch(`${serverUrl}/songs`, {
+        method: 'POST',
+        body: body,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(json => {
+        toggleLoader(false);
+        console.log(`done in - ${(Date.now() - start) / 1000}s`);
+
+        fetch(`${serverUrl}/playlist`).then(res => res.json()).then((albumInfo: TracklistInfo) => {
+            console.log('playlist from server');
+            generatePlaylist(albumInfo.playlist, albumInfo.albumName);
+            addDownloadLinks(albumInfo.albumName);
+        });
+    });
+});
+
+function toggleLoader(startLoader: boolean): void {
+    if (startLoader) {
+        loader.classList.remove('hidden');
+        container.classList.add('blur');
+    } else {
+        loader.classList.add('hidden');
+        container.classList.remove('blur');
+    }
+}
+
+function addDownloadLinks(albumName: string): void {
+    const downloadBtnCollection = document.querySelectorAll('.download-btn');
+
+    for (let downloadBtn of downloadBtnCollection) {
+        downloadBtn.addEventListener('click', (event) => {
+            const target: HTMLButtonElement = <HTMLButtonElement>event.target;
+            window.location.href = `${serverUrl}/download?albumName=${albumName}&songName=${target.id}`;
+        });
+    }
+}
+
+function generatePlaylist(playlistArr: Playlist[], albumName: string): void {
+    const tracklistEl = document.querySelector('.tracklist');
+    let albumTitle = <Element>playlistEl.querySelector('h2').cloneNode();
+    albumTitle.textContent = albumName;
+    albumTitle.classList.remove('hidden');
+    let tracklistCopy: HTMLDivElement;
+    let currentTrack: Playlist;
+    let downloadBtn: HTMLButtonElement;
+    let tumbnailEl: HTMLImageElement;
+    let songEl: HTMLSpanElement;
+
+    for (let i = 0; i < playlistArr.length; i++) {
+        currentTrack = playlistArr[i];
+        tracklistCopy = <HTMLDivElement>tracklistEl.cloneNode(true);
+        tumbnailEl = tracklistCopy.querySelector('img');
+        songEl = tracklistCopy.querySelector('.song-info');
+        downloadBtn = tracklistCopy.querySelector('.download-btn');
+
+        tracklistCopy.classList.remove('hidden');
+        downloadBtn.id = currentTrack.songName;
+        songEl.textContent = `${currentTrack.songName} - ${currentTrack.songBegin}`;
+        tumbnailEl.src = currentTrack.tumbnail;
+
+        console.log(tracklistCopy);
+        playlistEl.classList.remove('hidden');
+        playlistEl.appendChild(tracklistCopy);
+    }
+}
+
+// generatePlaylist(playlist.playlist, playlist.albumName);
