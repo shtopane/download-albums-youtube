5193156f79c2d963806571e714ea0d191fbe88fd
kkirilova <vechesumstudent@gmail.com>
2019-11-16 15:33:08 +0200

    started working on download playlist

--- download-albums-youtube-fe/src/app/download-playlist/containers/download-playlist/download-playlist.component.ts
+++ download-albums-youtube-fe/src/app/download-playlist/containers/download-playlist/download-playlist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-download-playlist',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./download-playlist.component.scss']
 })
 export class DownloadPlaylistComponent implements OnInit {
+  public downloadPlaylistForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.downloadPlaylistForm = this.fb.group({
+      url: ['', Validators.required]
+    })
+  }
+
+  public onSubmit(event) {
+    event.preventDefault();
   }
 
 }
