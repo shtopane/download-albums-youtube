49d1a2cd11964c4c54aaebf22fe9bcbede0769a2
shtopane <vechesumstudent@gmail.com>
2020-01-31 20:16:50 +0200

    cross origin header

--- server/server.ts
+++ server/server.ts
@@ -10,18 +10,17 @@ if (port == null || port === '') {
   port = '4000';
 }
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'output')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
+
 app.listen(port, () => {
     console.log('Server on port: ' + port);
 });
